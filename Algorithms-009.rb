1f1bc804de96efd89778c085b286649b01d35cff
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-11 19:38:29 -0300

    Update Algorithms-009.rb

--- Algorithms-009.rb
+++ Algorithms-009.rb
@@ -1,156 +1,125 @@
-#Jogo da velha
-
-class Cell
-    attr_accessor :value
-
-    def initialize(value = " ")
-        @value = value
-    end
-
-    def empty?
-        @value == " "
-    end
-end
-
 class Board
-    def initialize 
-        @grid = Array.new(3) { Array.new(3) {Cell.new}}
-        @player_x = Player.new(:x)
-        @player_o = Player.new(:o)
+    def initialize
+      @grid = Array.new(3) { Array.new(3, " ") }
+    end
+  
+    def display
+      @grid.each { |row| puts row.join(" | ") }
+      puts "----+---+----"
     end
 
-    def empty?(x,y)
-        @grid[x][y].empty?
+    #def display
+    #    @grid.each_with_index do |row, i|
+    #        puts " #{row[0].value}  | #{row[1].value} | #{row[2].value} "
+    #        puts "----+---+----" if i < 2
+    #    end
+    #end
+  
+    def empty?(x, y)
+      @grid[x][y] == " "
     end
-
+  
+    def place_mark(x, y, mark)
+      @grid[x][y] = mark
+    end
+  
     def full?
-        @grid.flatten.none?(&:empty?)
+      @grid.flatten.none? { |cell| cell == " " }
     end
-
-    def set_cell(x, y, value)
-        @grid[x][y].value = value
+  
+    def winner?(mark)
+      check_rows(mark) || check_columns(mark) || check_diagonals(mark)
     end
-
-    def get_cell(x, y)
-        @grid[x][y]
-    end
-
-    def display_board
-        @grid.each_with_index do |row, i|
-            puts " #{row[0].value}  | #{row[1].value} | #{row[2].value} "
-            puts "----+---+----" if i < 2
-        end
-    end
-
-    def game_over
-        return :winner if winner?
-        return :draw if draw?
-        false
-    end
-
+  
     private
-
-    def winner?
-        (0..2).any? { |row| @grid[row].uniq.length == 1 && @grid[row][0]!= " " }
-
-        (0..2).any? { |col| [@grid[0][col], @grid[1][col], @grid[2][col]].uniq.length == 1 && @grid[0][col] != " "}
-
-        [
-            [@grid[0][0], @grid[1][1],@grid[2][2]].uniq.length == 1  && @grid[0][0] != " ",
-            [@grid[0][2], @grid[1][1],@grid[2][0]].uniq.length == 1  && @grid[0][2] != " "
-        ].any?
+  
+    def check_rows(mark)
+      @grid.any? { |row| row.all? { |cell| cell == mark } }
     end
-
-    def draw?
-        @grid.all? { |row| row.none? { |cell| cell == " "}} && !winner?
+  
+    def check_columns(mark)
+      @grid.transpose.any? { |col| col.all? { |cell| cell == mark } }
     end
-end
-
-class TicTacToe
-    attr_reader :board, :current_player
-
-    def initialize(player_x, player_o)
-        @board = Board.new
-        @current_player = [player_x, player_o].sample
+  
+    def check_diagonals(mark)
+      diagonals = [
+        [@grid[0][0], @grid[1][1], @grid[2][2]],
+        [@grid[0][2], @grid[1][1], @grid[2][0]]
+      ]
+      diagonals.any? { |diag| diag.all? { |cell| cell == mark } }
     end
-
-    def play
-        players = Player.input_names
-        board = Board.new
-        game = TicTacToe.new(players[0], players[1])
-      
-        loop do
-          game.board.display_board
-          puts "#{game.current_player.name}: Entre com um número entre 1 e 9 para fazer sua jogada"
-          position = gets.chomp.to_i
-          x, y = game.convert_position_to_coordinates(position)
-          if game.valid_move?(game.board, x, y)
-            game.make_move(x, y, game.current_player.token)
-            if game.game_over?
-              game.board.display_board
-              if game.winner?
-                puts "Parabéns #{game.current_player.name}, você venceu!"
-              else
-                puts "Empatou!"
-              end
-              break
-            end
-          end
-          game.switch_player
-        end
+  end
+  
+  class Player
+    attr_reader :name, :mark
+  
+    def initialize(name, mark)
+      @name = name
+      @mark = mark
+    end
+  
+    def make_move(board)
+      puts "#{@name}, it's your turn (1-9):"
+      position = gets.chomp.to_i - 1
+      x, y = position.divmod(3)
+      until board.empty?(x, y)
+        puts "Invalid move. Try again:"
+        position = gets.chomp.to_i - 1
+        x, y = position.divmod(3)
       end
-
+      board.place_mark(x, y, @mark)
+    end
+  end
+  
+  class TicTacToe
+    def initialize(player_x, player_o)
+      @board = Board.new
+      @players = { x: player_x, o: player_o }
+      @current_player = @players[:x]
+    end
+  
+    def play
+      loop do
+        @board.display
+        @current_player.make_move(@board)
+        break if game_over?
+  
+        switch_player
+      end
+  
+      @board.display
+      announce_result
+    end
+  
     private
-
-    def convert_position_to_coordinates(position)
-        x = (position - 1) / 3
-        y = (position - 1) % 3
-        [x, y]
-    end
-
-    def valid_move?(board, x, y)
-        board.empty?(x, y)
-    end
-
-    def make_move(x, y, token)
-        @board.set_cell(x, y, token)
-    end
-
-    def game_over?
-        winner? || @board.full?
-    end
-
-    def winner?
-        @board.winner?
-    end
-
+  
     def switch_player
-        @current_player = @current_player == @board.player_x ? @board.player_o : @board.player_x
+      @current_player = (@current_player == @players[:x]) ? @players[:o] : @players[:x]
     end
-
-end
-
-class Player
-    attr_accessor :name, :token
-
-    def initialize(token, name = nil)
-        @token = token
-        @name = name
+  
+    def game_over?
+      @board.full? || @board.winner?(@current_player.mark)
     end
-
-    def token
-        @token
+  
+    def announce_result
+      if @board.winner?(@current_player.mark)
+        puts "#{@current_player.name} wins!"
+      else
+        puts "It's a draw!"
+      end
     end
-
-    def self.input_names
-        puts "Insira o nome do jogador que ira jogar com #{:x}:"
-        x_name = gets.chomp
-        puts "Insira o nome do jogador que ira jogar com #{:o}:"
-        o_name = gets.chomp
-        [Player.new(:x, x_name), Player.new(:o, o_name)]
-    end
-end
-
-players = Player.input_names
-game = TicTacToe.new(players[0], players[1])
-game.play
+  end
+  
+  # Main
+  puts "Welcome to Tic Tac Toe!"
+  puts "Player X, enter your name:"
+  name_x = gets.chomp
+  puts "Player O, enter your name:"
+  name_o = gets.chomp
+  
+  player_x = Player.new(name_x, "X")
+  player_o = Player.new(name_o, "O")
+  
+  game = TicTacToe.new(player_x, player_o)
+  game.play
+  
