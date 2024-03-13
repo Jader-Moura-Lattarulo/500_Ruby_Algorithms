49672658001ce58cec0f6de6f289cefce9087df1
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-13 12:37:05 -0300

    Update Algorithms-019.rb

--- Algorithms-019.rb
+++ Algorithms-019.rb
@@ -1,13 +1,17 @@
 print "Qual número você quer utilizar para as funções numéricas? "
 user_input = gets.chomp.to_i
-
+def pow (base, exp)
+    result = base ** exp
+    return result
+end
+    
 def numeric_functions(input)
     puts "Raiz: #{Math.sqrt(input)}"
 
-    real_int = (Math.power(1/2.0, Math.log(input))).to_i
+    real_int = (pow(1/2.0, Math.log(input))).to_i
     puts "Raiz com exponencial, log e conversão de real para inteiro: #{real_int}"
 
-    puts "Raiz com exponencial e log, sem conversão de real para inteiro: #{(Math.exp(1/2.0 * Math.log(input)))}"
+    puts "Raiz com exponencial e log, sem conversão de real para inteiro: #{(pow(1/2.0, Math.log(input)))}"
 
     
 end
