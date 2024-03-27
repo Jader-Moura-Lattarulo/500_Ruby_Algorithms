d6073df45ef14124a0b7e6a450d873d1dc1faf15
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-27 13:23:41 -0300

    Update Algorithms-055.rb

--- Algorithms-055.rb
+++ Algorithms-055.rb
@@ -1,13 +1,28 @@
 #Algoritmo qeu calcula a area de um losango
 
+def get_measurements
+    print "Medida da diagonal maior: "
+    major_diagonal = gets.chomp.to_f.round(2)
+
+    print "Medida da diagonal menor: "
+    minor_diagonal = gets.chomp.to_f.round(2)
+
+    validate_measurements(major_diagonal, minor_diagonal)
+end
+
+def validate_measurements(major_diagonal,minor_diagonal)
+    if major_diagonal < minor_diagonal
+        puts "A medida da diagonal maior deve ser MAIOR que a medida da diagonal menor."
+        get_measurements
+    end
+    area_of_rhombus(major_diagonal, minor_diagonal)
+    return major_diagonal, minor_diagonal
+end
+
 def area_of_rhombus(major_diagonal, minor_diagonal) 
     return (major_diagonal * minor_diagonal)/2
 end
 
-print "Medida da diagonal maior: "
-major_diagonal = gets.chomp.to_f.round(2)
-
-print "Medida da diagonal menor: "
-minor_diagonal = gets.chomp.to_f.round(2)
+major_diagonal, minor_diagonal = get_measurements
 
 puts "A area do losango é: #{area_of_rhombus(major_diagonal, minor_diagonal).round(2)}"
