d3ed988661d85c754bbb50f93df79b57097a978a
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-27 12:42:30 -0300

    Update Algorithms-053.rb

--- Algorithms-053.rb
+++ Algorithms-053.rb
@@ -1,2 +1,16 @@
 # Entrar com os lados de um paralelepípedo, calcular a diagonal.
 
+def diagonal_of_parallelepiped(height, base, depth)
+    return Math.sqrt( height ** 2 + base ** 2 + depth ** 2 )
+end
+
+print "Entre com a altura: "
+height = gets.chomp.to_f.round(2)
+
+print "Entre com a base: "
+base = gets.chomp.to_f.round(2)
+
+print "Entre com a profundidade: "
+depth = gets.chomp.to_f.round(2)
+
+puts "Diagonal do paralelepípedo é: #{diagonal_of_parallelepiped(height, base, depth).round(2)}"
