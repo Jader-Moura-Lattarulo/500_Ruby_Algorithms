c5d7fdde9082fb250f3140cdd60f2db01fdd7416
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-27 14:01:59 -0300

    Update Algorithms-054.rb

--- Algorithms-054.rb
+++ Algorithms-054.rb
@@ -1,4 +1,34 @@
 #Algoritimo que calcula e imprime a area de um triângulo.
 
-print "Entre com a base do triangulo: "
-base_of_triangle = gets.chomp.round(2)
+def clear_console
+    system('clear') || system('cls')
+end
+
+def validate_measurements(base_of_triangle, height_of_triangle)
+    if base_of_triangle == 0 || height_of_triangle == 0
+        clear_console
+        puts "A area só pode ser calculada com valores positivos."
+        puts "Insira valores validos."
+        get_measurements
+    end
+    area_of_triangle(base_of_triangle, height_of_triangle)
+    return base_of_triangle, height_of_triangle
+end
+
+def area_of_triangle(base_of_triangle, height_of_triangle)
+    return (base_of_triangle * height_of_triangle)/2
+end
+
+def get_measurements
+    print "Entre com a base do triangulo: "
+    base_of_triangle = gets.chomp.to_f.round(2)
+
+    print "Entre com a altura do triangulo: "
+    height_of_triangle = gets.chomp.to_f.round(2)
+
+    validate_measurements(base_of_triangle, height_of_triangle)
+end
+
+base_of_triangle, height_of_triangle = get_measurements
+
+puts "A area de um triangulo de base #{base_of_triangle} e altura #{height_of_triangle} é de #{area_of_triangle(base_of_triangle, height_of_triangle).round(2)}"
