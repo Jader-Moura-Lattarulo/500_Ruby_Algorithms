8686733e737b7095b6846fd6245812d7cc5a6ad9
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-28 15:02:05 -0300

    Update Algorithms-057.rb

--- Algorithms-057.rb
+++ Algorithms-057.rb
@@ -1,41 +1,55 @@
 #Entrar com as notas das provas 1 e 2 e imprimir a média final, arredondada e real
 
 class Test
-    attr_accessor
+    attr_accessor :test01, :test02, :real_average, :rounded_average
 
-    def initialize (test01,test02)
+    def initialize (test01, test02, real_average, rounded_average)
         @test01 = test01
         @test02 = test02
+        @real_average = real_average
+        @rounded_average = rounded_average
+
     end
 
-    def print_results
+    def print_results(real_average, rounded_average)
         puts
         puts "+~^~^~^~^~^~^~^~^~^~^~+"
-        puts "Média Real = #{}"
+        puts "Média Real = #{@real_average}"
         puts
-        puts "Média Arredondada = #{}"
+        puts "Média Arredondada = #{@rounded_average}"
         puts "+~^~^~^~^~^~^~^~^~^~^~+"
     end
 end
 
-def valid_grade
-    print "Digite a nota que tirou na primeira prova: "
-    grade01 = gets.chomp.to_f
-
-    if grade01 >= 0.00 && grade01 <= 10.00
-    return test01 if grade01 >= 0.00 && grade01 <= 10.00
-    
-    puts "Nota inválida, a nota deve ser entre 0 e 10, por favor insira uma nota valida."
-
-    print "Digite a nota que tirou na segunda prova: "
-    grade02 = gets.chomp.to_f
-
-    return test01 if grade01 >= 0.00 && grade01 <= 10.00
-    
-    puts "Nota inválida, a nota deve ser entre 0 e 10, por favor insira uma nota valida."
+def valid_grade (grade)
+    if grade >= 0.00 && grade <= 10.00
+        return grade
+    else
+        puts "Nota inválida, a nota deve ser entre 0 e 10, por favor insira uma nota valida."
+        grade = gets.chomp.to_f
+        valid_grade(grade)
+    end
 end
 
-test01, test02 = valid_grade
+def average (test01, test02)
+    real_average = (test01+test02)/2
+    rounded_average = real_average.round
+    return real_average, rounded_average
+end
+
+print "Digite a nota que tirou na primeira prova: "
+grade = gets.chomp.to_f
+valid_grade(grade)
+test01 = grade
+
+print "Digite a nota que tirou na segunda prova: "
+grade = gets.chomp.to_f
+valid_grade(grade)
+test02 = grade
+
+average(test01, test02)
+real_average = real_average
+rounded_average = rounded_average
 
 test = Test.new(test01,test02)
-test.print_results
+test.print_results(real_average, rounded_average)
