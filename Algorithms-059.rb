bf23668485e8dc15ec6340c91f203bfe3a3ac55b
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-29 14:04:10 -0300

    Update Algorithms-059.rb

--- Algorithms-059.rb
+++ Algorithms-059.rb
@@ -1,19 +1,20 @@
 #Algoritmo que entra com os valores dos catetos de um triângulo retângulo e imprimir a hipotenusa.
+#Algorithm that enters the values ​​of the legs of a right-angled triangle and prints the hypotenuse.
 
 class RightTriangle
     attr_accessor :cathetus01, :cathetus02, :hypotenuse
 
-    def initialize (cathetus01, cathetus02, hypotenuse)
+    def initialize (cathetus01, cathetus02)
         @cathetus01 = cathetus01
         @cathetus02 = cathetus02
         @hypotenuse = Math.sqrt (cathetus01 ** 2 + cathetus02 ** 2)
     end
 
-    def print_results(hypotenuse)
+    def print_result
         puts
         puts "+~^~^~^~^~^~^~^~^~^~^~+"
         puts 
-        puts "A hipotenusa é: #{hypotenuse.round(2)}"
+        puts "A hipotenusa é: #{@hypotenuse.round(2)}"
         puts 
         puts "+~^~^~^~^~^~^~^~^~^~^~+"
     end
@@ -24,9 +25,21 @@ def validate_side(side)
         return side
     else
         puts "#{side} não é um número, pfv insira um valor válido: "
+        side = gets.chomp.to_f
+        validate_side(side)
+    end
+end
+
+def get_side
+    side = gets.chomp.to_f
+    return side
+end
 
 print "Entre com o 1º cateto: "
-cathetus01 = gets.chomp.to_f
-
+cathetus01 = validate_side(get_side)
+ 
 print "Entre com o 2º cateto: "
-cathetus02 = gets.chomp.to_f
+cathetus02 = validate_side(get_side)
+
+righttriangle = RightTriangle.new(cathetus01, cathetus02)
+righttriangle.print_result
