49feed1b196272b3748cb2f15c2bbb673295b913
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-29 14:13:48 -0300

    Update Algorithms-060.rb

---
+++ Algorithms-060.rb
@@ -0,0 +1,10 @@
+#Algoritmo para entrar com uma razão de progressão aritimética e o valor do 1º termo. Ele calcula e imprime o 10º termo da série.
+#Algorithm to enter an arithmetic progression ratio and the value of the 1st term. It calculates and prints the 10th term of the series.
+
+print "Entre com o 1º termo: "
+first_term = gets.chomp.to_i
+
+print "Entre com a razão: "
+ratio = gets.chomp.to_i
+
+tenth_term
