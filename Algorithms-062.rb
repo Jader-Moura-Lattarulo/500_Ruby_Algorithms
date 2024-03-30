659e01b397e444f3b5babe6c010c960ccde64104
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-30 20:40:55 -0300

    Update Algorithms-062.rb

--- Algorithms-062.rb
+++ Algorithms-062.rb
@@ -1,13 +1,33 @@
 #Algoritmo que permite entrar com o valor de um produto e imprime o novo valor com um desconto de 9%.
 #Algorithm that allows you to enter the value of a product and print the new value with a 9% discount.
+require 'date'
 
 class Product 
-    attr_accessor :product_name, :price, :discount_price 
+    attr_accessor :product_name, :price, :discount, :discount_price, :today 
 
-    def initialize
+    def initialize (product_name, price)
         @product_name = product_name
         @price = price
+        @today = Data.today
+        @discount = price * 0.09
         @discount_price = price * 0.91
+    end
+
+    def print_result
+        puts
+        puts "          NOTA FISCAL          "
+        puts "-------------------------------"
+        puts "|   Produto:       |   Preço:  |"
+        puts "-------------------------------"
+        puts "|   #{product_name}          |   R$ 2.50 |"
+        puts "|                  |           |"
+        puts "|   Desconto        |   R$ #{discount} |"
+        puts "-------------------------------"
+        puts "|   Total:         |   R$ #{discount_price} |"
+        puts "-------------------------------"
+        puts "|   Data: #{today}           |"
+        puts "-------------------------------"
+        puts
     end
 end
 
@@ -21,9 +41,14 @@ def validate_price(user_input)
 end
 
 def get_price
-
+    user_input = gets.chomp
+    until validate_price(user_input)
+        puts "#{user_input} não é uma entrada valida, favor insira um valor real."
+        user_input = gets.chomp
+    end
+    price = user_input.chomp.to_f
+    return price
 end
-
 
 print "Entre com o nome do produto: "
 product_name = gets.chomp
