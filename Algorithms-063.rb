cfe9e31b831e2aaaeff05d10b98aa010fc890f86
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-31 15:43:52 -0300

    Update Algorithms-063.rb

---
+++ Algorithms-063.rb
@@ -0,0 +1,10 @@
+#Algoritmo que efetua o cálculo do salário líquido de um professor. Os dados fornecidos serão: valor da hora aula, número de aulas dadas no mês, hora atividade, adicional noturno caso a aula passe das 22h e percentual de desconto do INSS.
+#Algorithm that calculates a teacher's net salary. The data provided will be: hourly class fee, number of classes given in the month, activity time, additional night time if the class goes beyond 10pm and SSA discount percentage.
+
+print "Insira as horas trabalhadas: "
+worked_hours = gets.chomp.to_f
+
+print "Insira o valor da hora-aula: "
+hourly_wage = gets.chomp.to_f
+
+print "Insira o valor de horas após 22h"
