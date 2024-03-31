2d0c6c83ef322675479d1bd63e8a46695a975056
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-31 16:39:30 -0300

    Update Algorithms-063.rb

--- Algorithms-063.rb
+++ Algorithms-063.rb
@@ -12,4 +12,24 @@ hours_after_10pm = gets.chomp.to_f
 
 additional_night = hours_after_10pm * hourly_wage * 0.25
 
-gross_salary = (worked_hours * hourly_wage) + (additional_night) 
+gross_salary = (worked_hours * hourly_wage) + additional_night
+
+case gross_salary
+when 0..1100.0
+    puts "faixa 01: 7,5%"
+    inss = 1100 * 0.075
+when 1100.01..2203.48
+    puts "faixa 02: 9%" 
+    count = (2203.48 - 1100) * 0.09
+    inss = inss + count
+when 2203.49..3305.22
+    puts "faixa 03: 12%"
+    count = (3305.22 - 2203.48) * 0.12
+    inss = inss + count
+when 3305.23..6433.57
+    puts "faixa 04: 14%"
+    count = (6433.57 - 3305.23) * 0.14
+    inss = inss + count
+else
+    puts "Outro regime"
+end
