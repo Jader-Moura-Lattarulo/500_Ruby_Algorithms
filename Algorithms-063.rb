2905dbcecbd3b247f8884c3de72edd8f87582545
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-31 23:49:16 -0300

    Update Algorithms-063.rb

--- Algorithms-063.rb
+++ Algorithms-063.rb
@@ -1,6 +1,36 @@
 #Algoritmo que efetua o cálculo do salário líquido de um professor. Os dados fornecidos serão: valor da hora aula, número de aulas dadas no mês, hora atividade, adicional noturno caso a aula passe das 22h e percentual de desconto do INSS.
 #Algorithm that calculates a teacher's net salary. The data provided will be: hourly class fee, number of classes given in the month, activity time, additional night time if the class goes beyond 10pm and SSA discount percentage.
 
+class PayStub 
+    attr_accessor :worked_hours, :hourly_wage, :hours_after_10pm, ; additional_night, ;gross_salary
+
+    def initialize(worked_hours, hourly_wage, hours_after_10pm)
+        @worked_hours = worked_hours
+        @hourly_wage = hourly_wage
+        @hours_after_10pm = hours_after_10pm
+        @additional_night = hours_after_10pm * hourly_wage * 0.25
+        @gross_salary = (worked_hours * hourly_wage) + additional_night
+    end
+
+    def print_pay_stub
+        puts
+        puts "---------------------------------"
+        puts "|           HOLERITE            |"
+        puts "|-------------------------------|"
+        puts "|   Descrição\t|   Valor:  \t|"
+        puts "|-------------------------------|"
+        puts "|   Horas Trabalhadas\t|   #{format('%.2f', worked_hours)}\t|"
+        puts "|           \t|           \t|"
+        puts "|   Desconto\t|   R$ #{format('%.2f', discount)}\t|"
+        puts "|-------------------------------|"
+        puts "|   Total:\t|   R$ #{format('%.2f', discount_price)}\t|"
+        puts "|-------------------------------|"
+        puts "|   \tData: #{today}\t|"
+        puts "---------------------------------"
+        puts
+    end
+end
+
 print "Insira as horas trabalhadas: "
 worked_hours = gets.chomp.to_f
 
@@ -10,11 +40,7 @@ hourly_wage = gets.chomp.to_f
 print "Insira o valor de horas após 22h: "
 hours_after_10pm = gets.chomp.to_f
 
-additional_night = hours_after_10pm * hourly_wage * 0.25
-
-gross_salary = (worked_hours * hourly_wage) + additional_night
-
-case gross_salary
+case inss (gross_salary)
 when 0..1100.0
     puts "faixa 01: 7,5%"
     puts "#{gross_salary}"
@@ -32,9 +58,7 @@ when 1100.01..2203.48
 when 2203.49..3305.22
     puts "faixa 03: 12%"
     puts "#{gross_salary}"
-    inss = 1100 * 0.075
-    count = (2203.48 - 1100) * 0.09
-    inss = inss + count
+    inss = (1100 * 0.075) + (1103.48 * 0.09)
     count = (gross_salary - 2203.48) * 0.12
     inss = inss + count
     puts "#{inss}"
@@ -42,15 +66,15 @@ when 2203.49..3305.22
 when 3305.23..6433.57
     puts "faixa 04: 14%"
     puts "#{gross_salary}"
-    inss = 1100 * 0.075
-    count = (2203.48 - 1100) * 0.09
-    inss = inss + count
-    count = (3305.22 - 2203.48) * 0.12
-    inss = inss + count
+    inss = (1100 * 0.075) + (1103.48 * 0.09) + (1101.74 * 0.12)
     count = (gross_salary - 3305.23) * 0.14
     inss = inss + count
     puts "#{inss}"
     puts "#{gross_salary - inss}"
 else
-    puts "Outro regime"
+    puts "faxa 05: 14%"
+    puts "#{gross_salary}"
+    inss = (1100 * 0.075) + (1103.48 * 0.09) + (1101.74 * 0.12) + (3305.23 * 0.14)
+    puts "#{inss}"
+    puts "#{gross_salary - inss}"
 end
